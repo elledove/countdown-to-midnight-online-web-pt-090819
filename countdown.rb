00402ce4bb316c91262203de73771eae3b644807
Lauren Dove <elledoveinc@gmail.com>
2019-09-14 00:50:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
-  sleep 1
+  sleep 1 #CANT BELIEVE I GOT THIS RIGHT ON THR FIRST ATTEMPT!!!!!!
   number -= 1 
  end
 end
