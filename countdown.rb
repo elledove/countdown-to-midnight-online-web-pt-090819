2c17764752d5b0a631ee3466d933a6e87e9f71a4
Lauren Dove <elledoveinc@gmail.com>
2019-09-14 00:40:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown
-  number #= 10
-  while number #== 10
-   puts "#{number} SECOND(S)!"
+  number = 10
+  while number == 10
+   return "#{number} SECOND(S)!"
    number -= 1
   end
 end
