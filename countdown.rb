1cdbf22590fe46e5a0833f54838c0205860c1100
Lauren Dove <elledoveinc@gmail.com>
2019-09-14 00:43:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,8 @@ def countdown(number)
   while number > 0
    puts "#{number} SECOND(S)!"
    number -= 1
+   
   end
+  return "HAPPY NEW YEAR!"
 end
-return "HAPPY NEW YEAR!"
+#return "HAPPY NEW YEAR!"
